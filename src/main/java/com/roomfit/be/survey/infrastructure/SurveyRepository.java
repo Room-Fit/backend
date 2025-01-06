86b5ad266716c06b216ffc7b8b517f0cd277acb5
KNU-K <knukang334@gmail.com>
2025-01-07 05:25:20 +0900

    feat(survey-repository) : 커스텀 ReplyRepository 구현
    JdbcTempalate 을 통해서 Bulk Batch Insert 가 가능하도록 하여 성능 개선

--- src/main/java/com/roomfit/be/survey/infrastructure/SurveyRepository.java
+++ src/main/java/com/roomfit/be/survey/infrastructure/SurveyRepository.java
@@ -2,6 +2,7 @@ package com.roomfit.be.survey.infrastructure;
 
 import com.roomfit.be.survey.domain.Questionnaire;
 import com.roomfit.be.survey.infrastructure.support.QuestionnaireRepository;
+import com.roomfit.be.survey.infrastructure.support.ReplyRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +12,7 @@ import java.util.Optional;
  * QuestionnaireRepository, ReplyRepositoryCustom CustomRepository 를 통해서 Bulk 데이터 처리 최적화
  */
 @Repository
-public interface SurveyRepository extends JpaRepository<Questionnaire, Long>, QuestionnaireRepository {
+public interface SurveyRepository extends JpaRepository<Questionnaire, Long>, QuestionnaireRepository, ReplyRepository {
     /**
      * 가장 최근 설문지 -> Order by 로 인한 성능 저하 우려
      */
