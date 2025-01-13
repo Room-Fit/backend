6cd9b44a058e01309e16bfdc8775b66df30d322b
KNU-K <knukang334@gmail.com>
2025-01-13 20:28:42 +0900

    refactor(chat) : ChatRoomType 내에 타입 일치 유무 확인 로직 추가

--- src/main/java/com/roomfit/be/chat/domain/ChatRoomType.java
+++ src/main/java/com/roomfit/be/chat/domain/ChatRoomType.java
@@ -2,5 +2,12 @@ package com.roomfit.be.chat.domain;
 
 public enum ChatRoomType {
     GROUP,
-    PRIVATE
+    PRIVATE;
+    public static ChatRoomType fromString(String type) {
+        try {
+            return ChatRoomType.valueOf(type.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid chat room type: " + type, e);
+        }
+    }
 }
