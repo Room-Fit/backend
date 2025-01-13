7ba91a0dd68cafb00958324699d3c1481901e1b4
KNU-K <knukang334@gmail.com>
2025-01-13 19:35:17 +0900

    feat(chat-domain): chatRoom 도메인 구성
    id, name, type, status, maxQuota, currentQuota 구성

---
+++ src/main/java/com/roomfit/be/chat/domain/ChatRoomStatus.java
@@ -0,0 +1,7 @@
+package com.roomfit.be.chat.domain;
+
+public enum ChatRoomStatus {
+    RECRUITING,
+    RECRUITMENT_COMPLETE,
+    MATCH_COMPLETE
+}
---
+++ src/main/java/com/roomfit/be/chat/domain/ChatRoomType.java
@@ -0,0 +1,6 @@
+package com.roomfit.be.chat.domain;
+
+public enum ChatRoomType {
+    GROUP,
+    PRIVATE
+}
