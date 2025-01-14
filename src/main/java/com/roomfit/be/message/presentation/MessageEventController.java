c1ce7c9f1cf398d98502990f68b8a74d9aafccce
KNU-K <knukang334@gmail.com>
2025-01-15 03:28:49 +0900

    refactor(message-controller) : destination 경로를 path 로 받아서 처리하도록 변경

--- src/main/java/com/roomfit/be/message/presentation/MessageEventController.java
+++ src/main/java/com/roomfit/be/message/presentation/MessageEventController.java
@@ -3,19 +3,24 @@ package com.roomfit.be.message.presentation;
 import com.roomfit.be.message.application.MessageService;
 import com.roomfit.be.message.application.dto.MessageDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.index.qual.SameLen;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@Slf4j
 @RequiredArgsConstructor
 public class MessageEventController {
     private final SimpMessagingTemplate messagingTemplate;
     private final MessageService messageService;
 
-    @MessageMapping("/send")
-    public void sendMessage(MessageDTO.Send message) {
-        messagingTemplate.convertAndSend("/topic/room/" + message.getRoomId(), message);
+    @MessageMapping("/send/{roomId}")
+    public void sendMessage(@DestinationVariable Long roomId, MessageDTO.Send message) {
+        log.info(message.toString());
+        messagingTemplate.convertAndSend("/topic/room/" + roomId, message);
 
         messageService.sendMessage(message);
     }
