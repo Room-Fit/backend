3da1af2b16e35f0d0821427f1856abc87b3e2c43
KNU-K <knukang334@gmail.com>
2025-01-15 18:40:38 +0900

    feat(jwt-properties) : 내부 옵션에 대한 정보를 컴파일 시에 가지고 오도록 구성

---
+++ src/main/java/com/roomfit/be/auth/application/JwtProperties.java
@@ -0,0 +1,13 @@
+package com.roomfit.be.auth.application;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+@Data
+public class JwtProperties {
+    private String secretKey;  // 비밀 키
+    private long accessTokenExpirationTime;  // Access Token 유효 기간
+    private long refreshTokenExpirationTime;
+}
