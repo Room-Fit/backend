e96af4c9400933f976c0677e133363179f5deaba
KNU-K <knukang334@gmail.com>
2025-01-15 23:33:02 +0900

    refactor(auth-service) : token 내에 payload 고도화 [UserDetails]

--- src/main/java/com/roomfit/be/auth/application/AuthServiceImpl.java
+++ src/main/java/com/roomfit/be/auth/application/AuthServiceImpl.java
@@ -1,7 +1,10 @@
 package com.roomfit.be.auth.application;
 
+import com.roomfit.be.auth.application.domain.JwtTokenProvider;
 import com.roomfit.be.auth.application.dto.AuthDTO;
+import com.roomfit.be.auth.application.dto.UserDetails;
 import com.roomfit.be.email.EmailSendEvent;
+import com.roomfit.be.global.util.RandomCodeGenerator;
 import com.roomfit.be.redis.application.ReactiveRedisService;
 import com.roomfit.be.user.application.UserService;
 import com.roomfit.be.user.application.dto.UserDTO;
@@ -11,8 +14,6 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.security.SecureRandom;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -20,6 +21,7 @@ public class AuthServiceImpl implements AuthService{
     private final ApplicationEventPublisher applicationEventPublisher;
     private final ReactiveRedisService reactiveRedisService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final RandomCodeGenerator codeGenerator;
     private final UserService userService;
     @Override
     public AuthDTO.LoginResponse authenticate(AuthDTO.Login request) {
@@ -27,8 +29,10 @@ public class AuthServiceImpl implements AuthService{
         if(!request.getPassword().equals(foundUser.getPassword()))
              throw new RuntimeException("Not found");
 
-        String accessToken = jwtTokenProvider.generateAccessToken(request.getEmail());
-        String refreshToken = jwtTokenProvider.generateRefreshToken(request.getEmail());
+        UserDetails userDetails = UserDetails.of(foundUser.getId(), foundUser.getEmail(), foundUser.getNickname(), foundUser.getRole());
+
+        String accessToken = jwtTokenProvider.generateAccessToken(userDetails);
+        String refreshToken = jwtTokenProvider.generateRefreshToken(userDetails);
 
         reactiveRedisService.saveWithTTL(request.getEmail(), refreshToken, 300)
                 .subscribe();
@@ -36,11 +40,14 @@ public class AuthServiceImpl implements AuthService{
         return AuthDTO.LoginResponse.of(accessToken, refreshToken);
     }
 
+    /**
+     * TODO: 이 작업을 별도의 서비스나 작업 큐로 오프로드하여 AuthServiceImpl 클래스의 부하를 줄이는 방법도 고려할 수 있습니다.
+     */
     @Async("taskExecutor")
     @Override
     public void generateVerificationCode(String sessionId, String email) {
 
-        String code = generateRandomCode(5);
+        String code = codeGenerator.generateSecureRandomCode(5);
         reactiveRedisService.saveWithTTL(sessionId, code, 300)
                 .doOnTerminate(() -> {
                     String subject = "Verification Code";
@@ -57,18 +64,5 @@ public class AuthServiceImpl implements AuthService{
          */
         String savedCode = (String) reactiveRedisService.get(sessionId).block(); // block()을 사용하여 동기적으로 값 가져오기
         return savedCode != null && savedCode.equals(code);
-    }
-
-
-    private String generateRandomCode(int length) {
-        SecureRandom random = new SecureRandom();
-        StringBuilder code = new StringBuilder();
-
-        for (int i = 0; i < length; i++) {
-            int digit = random.nextInt(10); // 0~9 사이의 숫자 생성
-            code.append(digit);
-        }
-
-        return code.toString();
     }
 }
