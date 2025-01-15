b39fb8635bc1873124599ef55ac788ce99fffc76
KNU-K <knukang334@gmail.com>
2025-01-15 18:40:05 +0900

    config(jwt) : JwtProperties 내에 컴파일 단계 내에서 주입 받을 수 있도록 Config 구성

---
+++ src/main/java/com/roomfit/be/global/config/JwtConfig.java
@@ -0,0 +1,10 @@
+package com.roomfit.be.global.config;
+
+import com.roomfit.be.auth.application.JwtProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(JwtProperties.class)
+public class JwtConfig {
+}
