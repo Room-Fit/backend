97c8ddd604f326196a96ed5cee905aad4e99f046
KNU-K <knukang334@gmail.com>
2025-01-15 18:39:28 +0900

    feat(auth-service) : authenticate 메소드 추가 (로그인시 인증 처리)

--- src/main/java/com/roomfit/be/auth/application/AuthService.java
+++ src/main/java/com/roomfit/be/auth/application/AuthService.java
@@ -1,6 +1,9 @@
 package com.roomfit.be.auth.application;
 
+import com.roomfit.be.auth.application.dto.AuthDTO;
+
 public interface AuthService {
     void generateVerificationCode(String sessionId, String email);
     boolean verifyVerificationCode(String sessionId, String code);
+    AuthDTO.LoginResponse authenticate(AuthDTO.Login request);
 }
