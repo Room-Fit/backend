8d9bf99b534ee0146a218710b6af8af84d3f2bd2
KNU-K <knukang334@gmail.com>
2025-01-20 01:49:07 +0900

    config(swagger) : swagger UI 내에 인증 처리를 할 수 있도록 추가 구성
    * Bearer 토큰 인증

--- src/main/java/com/roomfit/be/global/config/SwaggerConfig.java
+++ src/main/java/com/roomfit/be/global/config/SwaggerConfig.java
@@ -1,21 +1,34 @@
 package com.roomfit.be.global.config;
 
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
-                .version("0.0.1")
+                .version("0.0.2")
                 .title("Room-fit Project swagger")
                 .description("룸핏 프로젝트에 대한 Swagger");
 
+
         return new OpenAPI()
-                .info(info);
+                .info(info)
+                .components(new Components())
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
