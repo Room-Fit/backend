73093621f808506fed41afac584057e1f1b6c5c6
KNU-K <knukang334@gmail.com>
2025-01-20 01:51:32 +0900

    fix(survey-controller) : Operation 과 Parameter를 추가하여 Swagger 고도화

--- src/main/java/com/roomfit/be/survey/presentation/SurveyController.java
+++ src/main/java/com/roomfit/be/survey/presentation/SurveyController.java
@@ -6,8 +6,7 @@ import com.roomfit.be.survey.application.SurveyService;
 import com.roomfit.be.survey.application.dto.QuestionnaireDTO;
 import com.roomfit.be.survey.application.dto.ReplyDTO;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,13 +21,13 @@ public class SurveyController {
     /**
      * 설문지 생성
      */
-    @Operation(summary = "새로운 설문지 생성", description = "이 API는 새로운 설문지를 생성합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "설문지 생성 성공"),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청")
-    })
+    @Operation(
+            summary = "새로운 설문지 생성",
+            description = "제공된 요청 객체를 기반으로 새로운 설문지를 생성합니다."
+    )
     @PostMapping("")
-    public ResponseEntity<CommonResponse<QuestionnaireDTO.Response>> createQuestionnaire(@RequestBody QuestionnaireDTO.Create request) {
+    public ResponseEntity<CommonResponse<QuestionnaireDTO.Response>> createQuestionnaire(
+            @RequestBody @Parameter(description = "설문지 생성 요청 객체", required = true) QuestionnaireDTO.Create request) {
         QuestionnaireDTO.Response response = surveyService.createQuestionnaire(request);
         CommonResponse<QuestionnaireDTO.Response> commonResponse = ResponseFactory.success(response, "설문지 생성 성공");
         return ResponseEntity
@@ -39,11 +38,10 @@ public class SurveyController {
     /**
      * 최신 설문지 조회
      */
-    @Operation(summary = "최신 설문지 조회", description = "이 API는 최신 설문지를 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "최신 설문지 조회 성공"),
-            @ApiResponse(responseCode = "404", description = "설문지가 존재하지 않음")
-    })
+    @Operation(
+            summary = "최신 설문지 조회",
+            description = "가장 최근에 생성된 설문지를 조회합니다."
+    )
     @GetMapping("")
     public ResponseEntity<CommonResponse<QuestionnaireDTO.Response>> readQuestionnaire() {
         QuestionnaireDTO.Response response = surveyService.readLatestQuestionnaire();
@@ -56,13 +54,13 @@ public class SurveyController {
     /**
      * 설문 답변 생성
      */
-    @Operation(summary = "설문 답변 생성", description = "이 API는 설문에 대한 답변을 생성합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "설문 답변 생성 성공"),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청")
-    })
+    @Operation(
+            summary = "설문지에 대한 답변 생성",
+            description = "제공된 요청 객체를 기반으로 설문지에 대한 답변을 생성합니다."
+    )
     @PostMapping("/reply")
-    public ResponseEntity<CommonResponse<QuestionnaireDTO.Response>> createReply(@RequestBody ReplyDTO.Create request) {
+    public ResponseEntity<CommonResponse<QuestionnaireDTO.Response>> createReply(
+            @RequestBody @Parameter(description = "설문 답변 생성 요청 객체", required = true) ReplyDTO.Create request) {
         QuestionnaireDTO.Response response = surveyService.createReply(request);
         CommonResponse<QuestionnaireDTO.Response> commonResponse = ResponseFactory.success(response, "설문 답변 생성 성공");
         return ResponseEntity
@@ -73,13 +71,13 @@ public class SurveyController {
     /**
      * 사용자별 설문 답변 조회
      */
-    @Operation(summary = "사용자별 설문 답변 조회", description = "이 API는 특정 사용자의 설문 답변을 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "사용자별 설문 답변 조회 성공"),
-            @ApiResponse(responseCode = "404", description = "사용자가 존재하지 않음")
-    })
+    @Operation(
+            summary = "특정 사용자의 설문 답변 조회",
+            description = "사용자 ID를 기반으로 해당 사용자의 설문지에 대한 답변을 조회합니다."
+    )
     @GetMapping("/reply")
-    public ResponseEntity<CommonResponse<QuestionnaireDTO.Response>> readReply(@RequestParam("user_id") Long id) {
+    public ResponseEntity<CommonResponse<QuestionnaireDTO.Response>> readReply(
+            @RequestParam("user_id") @Parameter(description = "조회하려는 사용자 ID", required = true, example = "123") Long id) {
         QuestionnaireDTO.Response response = surveyService.readReplyByUserId(id);
         CommonResponse<QuestionnaireDTO.Response> commonResponse = ResponseFactory.success(response, "사용자별 설문 답변 조회 성공");
         return ResponseEntity
