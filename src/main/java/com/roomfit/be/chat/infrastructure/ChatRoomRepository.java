2b8cf810c6c7fba80c9f64e8cecc548e704fb742
KNU-K <knukang334@gmail.com>
2025-01-20 20:33:43 +0900

    refactor(chatroom-repository) : 참여 중이 채팅방 내의 메시지 목록 조회 메소드 추가

--- src/main/java/com/roomfit/be/chat/infrastructure/ChatRoomRepository.java
+++ src/main/java/com/roomfit/be/chat/infrastructure/ChatRoomRepository.java
@@ -1,7 +1,15 @@
 package com.roomfit.be.chat.infrastructure;
 
 import com.roomfit.be.chat.domain.ChatRoom;
+import com.roomfit.be.chat.domain.Message;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    @Query("SELECT m FROM messages m WHERE m.chatRoom.id = :chatRoomId")
+    List<Message> findMessagesByChatRoomId(@Param("chatRoomId") Long chatRoomId);
 }
