89d0218bc0f4288e8c1f781d00107aaef275de09
KNU-K <knukang334@gmail.com>
2025-01-20 23:45:20 +0900

    feat(user-exception) : user 도메인 내에 Custom Exception 및 ErrorCode 구성

--- src/main/java/com/roomfit/be/user/application/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.roomfit.be.user.application.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
