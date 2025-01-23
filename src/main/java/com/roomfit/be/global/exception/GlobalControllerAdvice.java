526ff9ef39cdd72f99d55fb6a0de238488e04376
KNU-K <knukang334@gmail.com>
2025-01-24 07:43:26 +0900

    refactor(exception) : ControllerAdvice에서 RestControllerAdvice 변경(응답 무한루프 에러 해결)
    응답 무한 루프 에러 해결

--- src/main/java/com/roomfit/be/global/exception/GlobalControllerAdvice.java
+++ src/main/java/com/roomfit/be/global/exception/GlobalControllerAdvice.java
@@ -1,8 +1,27 @@
 package com.roomfit.be.global.exception;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import com.roomfit.be.global.response.CommonResponse;
+import com.roomfit.be.global.response.ResponseFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-public class GlobalControllerAdvice{
+@RestControllerAdvice
+public class GlobalControllerAdvice {
 
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<CommonResponse<Void>> handleApplicationException(ApplicationException ex) {
+        CommonResponse<Void> commonResponse = ResponseFactory.failure(ex.getMessage());
+        return ResponseEntity
+                .status(ex.getStatus())
+                .body(commonResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public CommonResponse<Void> handleGeneralException(Exception ex) {
+        return ResponseFactory.failure("서버에서 예상치 못한 오류가 발생했습니다.");
+    }
 }
