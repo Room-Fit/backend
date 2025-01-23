0e8179fa61d980cf1b6684a8e2ac3fd1b9a60558
KNU-K <knukang334@gmail.com>
2025-01-24 07:02:26 +0900

    config : TransactionConfig 를 통해 readOnly 사용을 가능하도록 구성
    - 조회 성능에 대한 개선을 위해서 이를 추가

---
+++ src/main/java/com/roomfit/be/global/config/TransactionConfig.java
@@ -0,0 +1,9 @@
+package com.roomfit.be.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionConfig {
+}
