e4cd1c6d2d2d077f1f50f829801dc26983df7ddd
KNU-K <knukang334@gmail.com>
2025-01-24 06:16:29 +0900

    feat(survey-exception) : survey 도메인 내에 reply, questionnaire Exception 추가

--- src/main/java/com/roomfit/be/survey/application/exception/ReplySaveFailureException.java
+++ src/main/java/com/roomfit/be/survey/application/exception/ReplySaveFailureException.java
@@ -3,8 +3,8 @@ package com.roomfit.be.survey.application.exception;
 import com.roomfit.be.global.exception.ApplicationException;
 import com.roomfit.be.global.exception.ErrorCode;
 
-public class QuestionnaireNotFoundException extends ApplicationException {
-    public QuestionnaireNotFoundException() {
-        super(ErrorCode.QUESTIONNAIRE_NOT_FOUND);
+public class ReplySaveFailureException extends ApplicationException {
+    public ReplySaveFailureException() {
+        super(ErrorCode.REPLY_SAVE_FAILURE);
     }
 }
