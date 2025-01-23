c22aed1078052098f486e47adf10b2f97b907741
KNU-K <knukang334@gmail.com>
2025-01-24 07:43:56 +0900

    refactor(survey-domain) : QuestionDTO 내에서 직렬화, 역직렬화 로직 추가

--- src/main/java/com/roomfit/be/survey/domain/QuestionType.java
+++ src/main/java/com/roomfit/be/survey/domain/QuestionType.java
@@ -1,8 +1,22 @@
 package com.roomfit.be.survey.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum QuestionType {
-    DOUBLE_SLIDER,
-    SLIDER,
-    CHECKBOX,
-    SELECTOR
+    SLIDER, DOUBLE_SLIDER, SELECTOR, CHECKBOX;
+
+    @JsonValue
+    public String getValue() {
+        return name();
+    }
+
+    @JsonCreator
+    public static QuestionType forValue(String value) {
+        try {
+            return valueOf(value);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Unknown enum value: " + value, e);
+        }
+    }
 }
