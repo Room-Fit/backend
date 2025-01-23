5c9b2d9eda14f530dee50ae2525662e83220bc14
KNU-K <knukang334@gmail.com>
2025-01-24 07:01:38 +0900

    fix(user) : response 형식 일관성 있게 변경

--- src/main/java/com/roomfit/be/user/presentation/UserController.java
+++ src/main/java/com/roomfit/be/user/presentation/UserController.java
@@ -7,7 +7,6 @@ import com.roomfit.be.user.application.dto.UserDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,13 +25,11 @@ public class UserController {
             description = "새로운 사용자를 생성합니다."
     )
     @PostMapping("")
-    public ResponseEntity<CommonResponse<UserDTO.Response>> create(
+    @ResponseStatus(HttpStatus.CREATED)  // 201 상태 코드
+    public CommonResponse<UserDTO.Response> create(
             @RequestBody UserDTO.Create request) {
         UserDTO.Response response = userService.create(request);
-        CommonResponse<UserDTO.Response> commonResponse = ResponseFactory.success(response, "사용자 생성 성공");
-        return ResponseEntity
-                .status(HttpStatus.CREATED)  // 201 상태 코드
-                .body(commonResponse);  // 응답 본문
+        return ResponseFactory.success(response, "사용자 생성 성공");
     }
 
     /**
@@ -43,12 +40,10 @@ public class UserController {
             description = "시스템에 등록된 모든 사용자를 조회합니다."
     )
     @GetMapping("")
-    public ResponseEntity<CommonResponse<List<UserDTO.Response>>> readAll() {
+    @ResponseStatus(HttpStatus.OK)  // 200 상태 코드
+    public CommonResponse<List<UserDTO.Response>> readAll() {
         List<UserDTO.Response> response = userService.readAll();
-        CommonResponse<List<UserDTO.Response>> commonResponse = ResponseFactory.success(response, "모든 사용자 조회 성공");
-        return ResponseEntity
-                .status(HttpStatus.OK)  // 200 상태 코드
-                .body(commonResponse);  // 응답 본문
+        return ResponseFactory.success(response, "모든 사용자 조회 성공");
     }
 
     /**
@@ -59,12 +54,10 @@ public class UserController {
             description = "특정 사용자의 ID로 사용자를 조회합니다."
     )
     @GetMapping("/{user_id}")
-    public ResponseEntity<CommonResponse<UserDTO.Response>> readById(
+    @ResponseStatus(HttpStatus.OK)  // 200 상태 코드
+    public CommonResponse<UserDTO.Response> readById(
             @PathVariable("user_id") Long id) {
         UserDTO.Response response = userService.readById(id);
-        CommonResponse<UserDTO.Response> commonResponse = ResponseFactory.success(response, "사용자 조회 성공");
-        return ResponseEntity
-                .status(HttpStatus.OK)  // 200 상태 코드
-                .body(commonResponse);  // 응답 본문
+        return ResponseFactory.success(response, "사용자 조회 성공");
     }
 }
