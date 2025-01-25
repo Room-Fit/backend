4893ebd233962b1fe4f825b00cd845f7c6a6fed3
KNU-K <knukang334@gmail.com>
2025-01-25 11:13:05 +0900

    refactor(chat-application): Lazy loading Initialization 문제를 해결하기 위해 트랜잭션 단위 리팩토링

--- src/main/java/com/roomfit/be/chat/application/ChatRoomServiceImpl.java
+++ src/main/java/com/roomfit/be/chat/application/ChatRoomServiceImpl.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ChatRoomServiceImpl implements ChatRoomService {
 
     private final ChatRoomRepository chatRepository;
