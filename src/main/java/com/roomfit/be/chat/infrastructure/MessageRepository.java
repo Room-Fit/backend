779bfa61cbb882c81a43a3b221bd01ee186a12a1
KNU-K <knukang334@gmail.com>
2025-01-25 09:15:53 +0900

    refactor(chat-infrastructure) : MessageRepository를 통해 채팅방에 존재하는 메시지 수를 가져오는 메소드 추가

--- src/main/java/com/roomfit/be/chat/infrastructure/MessageRepository.java
+++ src/main/java/com/roomfit/be/chat/infrastructure/MessageRepository.java
@@ -1,7 +1,13 @@
 package com.roomfit.be.chat.infrastructure;
 
 import com.roomfit.be.chat.domain.Message;
+import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    @Query("SELECT m.chatRoom.id AS roomId, COUNT(m) AS messageCount FROM messages m GROUP BY m.chatRoom.id")
+    List<Tuple> countMessagesByRoomIds();
 }
