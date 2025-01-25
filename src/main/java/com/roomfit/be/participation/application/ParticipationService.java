300fe442526a8a6684ef1e7cbbebb64763354415
KNU-K <knukang334@gmail.com>
2025-01-25 11:14:53 +0900

    refactor(participation-application) : ParticipationService 내에 트랜잭션 범위 재설정 및 응답 DTO 최적화

--- src/main/java/com/roomfit/be/participation/application/ParticipationService.java
+++ src/main/java/com/roomfit/be/participation/application/ParticipationService.java
@@ -1,6 +1,8 @@
 package com.roomfit.be.participation.application;
 
+import com.roomfit.be.chat.application.dto.ChatRoomDTO;
 import com.roomfit.be.chat.domain.ChatRoom;
+import com.roomfit.be.participation.application.dto.ParticipantDTO;
 import com.roomfit.be.user.domain.User;
 
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.List;
 public interface ParticipationService {
     void joinAsHost(Long userId, Long chatRoomId);
     void joinAsParticipant(Long userId, Long chatRoomId);
+    List<ParticipantDTO> readParticipantsInChatRoom(Long roomId);
 //    void removeParticipant(Long userId, Long chatRoomId);
-    List<User> readParticipantsByRoomId(Long roomId);
+//    List<User> readParticipantsByRoomId(Long roomId);
 }
--- src/main/java/com/roomfit/be/participation/application/ParticipationServiceImpl.java
+++ src/main/java/com/roomfit/be/participation/application/ParticipationServiceImpl.java
@@ -1,13 +1,16 @@
 package com.roomfit.be.participation.application;
 
+import com.roomfit.be.chat.application.dto.ChatRoomDTO;
 import com.roomfit.be.chat.domain.ChatRoom;
 import com.roomfit.be.chat.infrastructure.ChatRoomRepository;
+import com.roomfit.be.participation.application.dto.ParticipantDTO;
 import com.roomfit.be.participation.domain.Participation;
 import com.roomfit.be.participation.infrastructure.ParticipationRepository;
 import com.roomfit.be.user.domain.User;
 import com.roomfit.be.user.infrastructure.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,30 +21,35 @@ public class ParticipationServiceImpl implements  ParticipationService{
     private final UserRepository userRepository;
     private final ChatRoomRepository chatRoomRepository;
 
+    @Transactional
     @Override
     public void joinAsHost(Long userId, Long chatRoomId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
         ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
                 .orElseThrow(() -> new IllegalArgumentException("채팅방을 찾을 수 없습니다."));
-
         Participation participation = Participation.participateAsHost(user, chatRoom);
         participationRepository.save(participation);
     }
 
+    @Transactional
     @Override
     public void joinAsParticipant(Long userId, Long chatRoomId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
         ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
                 .orElseThrow(() -> new IllegalArgumentException("채팅방을 찾을 수 없습니다."));
-        
+
         Participation participation = Participation.participateAsParticipant(user, chatRoom);
         participationRepository.save(participation);
     }
 
     @Override
-    public List<User> readParticipantsByRoomId(Long roomId){
-        return participationRepository.findUsersByChatRoomId(roomId);
+    public List<ParticipantDTO> readParticipantsInChatRoom(Long roomId) {
+        return participationRepository.findUsersByChatRoomId(roomId)
+                .stream()
+                .map(ParticipantDTO::of)
+                .toList();
     }
+
 }
