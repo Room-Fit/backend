ee2a5605db6774ea2ceffe7abd5c0c2c66d3651d
KNU-K <knukang334@gmail.com>
2025-01-27 17:21:04 +0900

    refactor(survey-application) : reply 의 내부 MultipleBagFetchException 해결 (#68)

--- src/main/java/com/roomfit/be/survey/application/SurveyService.java
+++ src/main/java/com/roomfit/be/survey/application/SurveyService.java
@@ -3,15 +3,16 @@ package com.roomfit.be.survey.application;
 
 import com.roomfit.be.survey.application.dto.QuestionnaireDTO;
 import com.roomfit.be.survey.application.dto.ReplyDTO;
+import com.roomfit.be.survey.application.dto.SearchType;
 
 public interface SurveyService {
     QuestionnaireDTO.Response createQuestionnaire(QuestionnaireDTO.Create request);
 
     QuestionnaireDTO.Response createQuestionnaireLegacy(QuestionnaireDTO.Create request);
 
-    QuestionnaireDTO.Response createReply(ReplyDTO.Create request);
+    QuestionnaireDTO.Response createReply(Long userId, ReplyDTO.Create request);
 
-    QuestionnaireDTO.Response readLatestQuestionnaire();
+    QuestionnaireDTO.Response readLatestQuestionnaire(SearchType searchType);
 
     QuestionnaireDTO.Response readReplyByUserId(Long id);
 }
--- src/main/java/com/roomfit/be/survey/application/SurveyServiceImpl.java
+++ src/main/java/com/roomfit/be/survey/application/SurveyServiceImpl.java
@@ -3,16 +3,18 @@ package com.roomfit.be.survey.application;
 import com.roomfit.be.survey.application.dto.QuestionDTO;
 import com.roomfit.be.survey.application.dto.QuestionnaireDTO;
 import com.roomfit.be.survey.application.dto.ReplyDTO;
+import com.roomfit.be.survey.application.dto.SearchType;
 import com.roomfit.be.survey.application.exception.QuestionnaireNotFoundException;
 import com.roomfit.be.survey.application.exception.QuestionnaireSaveFailureException;
 import com.roomfit.be.survey.application.exception.ReplySaveFailureException;
 import com.roomfit.be.survey.domain.Question;
 import com.roomfit.be.survey.domain.Questionnaire;
+import com.roomfit.be.survey.infrastructure.QuestionRepository;
 import com.roomfit.be.survey.infrastructure.SurveyRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -26,6 +28,9 @@ import java.util.List;
 @Slf4j
 public class SurveyServiceImpl implements  SurveyService{
     private final SurveyRepository surveyRepository;
+    private final QuestionRepository questionRepository;
+    private Object questionDTOs;
+
     @Transactional
     @Override
     public QuestionnaireDTO.Response createQuestionnaire(QuestionnaireDTO.Create request) {
@@ -39,15 +44,22 @@ public class SurveyServiceImpl implements  SurveyService{
         }catch (Exception e){
             throw new QuestionnaireSaveFailureException();
         }
-        return readLatestQuestionnaire();
+        return readLatestQuestionnaire(SearchType.INCLUDE_SUB);
     }
 
     @Override
-    public QuestionnaireDTO.Response readLatestQuestionnaire() {
+    public QuestionnaireDTO.Response readLatestQuestionnaire(SearchType searchType) {
         Questionnaire foundQuestionnaire = surveyRepository.findTopByOrderByCreatedAtDesc()
                 .orElseThrow(QuestionnaireNotFoundException::new);
 
-        return QuestionnaireDTO.Response.of(foundQuestionnaire);
+        return switch (searchType) {
+            case INCLUDE_SUB-> QuestionnaireDTO.Response.of(foundQuestionnaire);
+            case EXCLUDE_SUB-> QuestionnaireDTO.Response.builder()
+                    .id(foundQuestionnaire.getId())
+                    .title(foundQuestionnaire.getTitle())
+                    .description(foundQuestionnaire.getDescription())
+                    .build();
+        };
     }
     @Transactional
     public QuestionnaireDTO.Response readLatestQuestionnaireWithReplies() {
@@ -71,9 +83,9 @@ public class SurveyServiceImpl implements  SurveyService{
 
     @Transactional
     @Override
-    public QuestionnaireDTO.Response createReply(ReplyDTO.Create request) {
+    public QuestionnaireDTO.Response createReply(Long userId, ReplyDTO.Create request) {
         try {
-            surveyRepository.saveBulkReply(request);
+            surveyRepository.saveBulkReply(userId, request);
         }catch (Exception e){
             throw new ReplySaveFailureException();
         }
@@ -83,11 +95,20 @@ public class SurveyServiceImpl implements  SurveyService{
     /**
      * TODO: 로그인 들어가면, user의 id 에 따라 수정
      */
+    @Transactional
     @Override
     public QuestionnaireDTO.Response readReplyByUserId(Long id) {
-        Questionnaire foundQuestionnaire = surveyRepository.findById(id)
-                .orElseThrow(()->new RuntimeException("Rely not exist"));
-        return QuestionnaireDTO.Response.ofWithReplies(foundQuestionnaire);
+        QuestionnaireDTO.Response questionnaireDTO = readLatestQuestionnaire(SearchType.EXCLUDE_SUB);
+        List<Question> questions = questionRepository.findByOwnerId(id, questionnaireDTO.getId());
+        if(questions.isEmpty()){
+            throw new RuntimeException("설문 진행해주세요");
+        }
+        List<QuestionDTO.Response> questionDTOs =questions.stream()
+                        .map(QuestionDTO.Response::of)
+                        .toList();
+        log.info(questionDTOs.toString());
+        questionnaireDTO.addQuestions(questionDTOs);
+        return questionnaireDTO;
     }
 
     private List<Question> convertToQuestions(List<QuestionDTO.Request> questions) {
