e7e906cc29f86edb6b7dca79470ac748c56bb3ab
KNU-K <knukang334@gmail.com>
2025-01-30 17:48:24 +0900

    fix(auth) : Verification으로 시작하는 도메인 Service Repository 들을 VerificationCode로 변경

--- src/main/java/com/roomfit/be/auth/application/VerificationCodeServiceImpl.java
+++ src/main/java/com/roomfit/be/auth/application/VerificationCodeServiceImpl.java
@@ -2,7 +2,7 @@ package com.roomfit.be.auth.application;
 
 import com.roomfit.be.auth.application.exception.VerificationCodeNotFoundException;
 import com.roomfit.be.auth.domain.VerificationCode;
-import com.roomfit.be.auth.infrastructure.VerificationRepository;
+import com.roomfit.be.auth.infrastructure.VerificationCodeRepository;
 import com.roomfit.be.email.application.EmailSendEvent;
 import com.roomfit.be.global.event.EventPublisher;
 import com.roomfit.be.global.util.RandomCodeGenerator;
@@ -12,9 +12,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class VerificationServiceImpl implements VerificationService {
+public class VerificationCodeServiceImpl implements VerificationCodeService {
     private static final int CODE_GENERATION_COUNT = 5;
-    private final VerificationRepository verificationRepository;
+    private final VerificationCodeRepository verificationCodeRepository;
     private final RandomCodeGenerator codeGenerator;
     private final EventPublisher eventPublisher;
 
@@ -24,14 +24,14 @@ public class VerificationServiceImpl implements VerificationService {
         String code = codeGenerator.generateSecureRandomCode(CODE_GENERATION_COUNT);
         VerificationCode verificationCode = VerificationCode.of(authToken, code);
         
-        verificationRepository.save(verificationCode)
+        verificationCodeRepository.save(verificationCode)
                 .doOnTerminate(() -> sendVerificationEmail(email, code))
                 .subscribe();
     }
 
     @Override
     public boolean verifyVerificationCode(String authToken, String code) {
-        VerificationCode verificationCode = verificationRepository.findByUUid(authToken)
+        VerificationCode verificationCode = verificationCodeRepository.findByUUid(authToken)
                 .orElseThrow(VerificationCodeNotFoundException::new);
 
         if (!verificationCode.isCodeValid(code)) {
@@ -45,13 +45,13 @@ public class VerificationServiceImpl implements VerificationService {
 
     @Override
     public boolean getStatus(String authToken) {
-        return verificationRepository.findStatusByAuthToken(authToken)
+        return verificationCodeRepository.findStatusByAuthToken(authToken)
                 .orElseThrow();
     }
 
     @Async("taskExecutor")
     protected void saveVerificationStatus(VerificationCode verificationCode) {
-        verificationRepository.update(verificationCode).subscribe();
+        verificationCodeRepository.update(verificationCode).subscribe();
     }
 
     //retriable 하게
